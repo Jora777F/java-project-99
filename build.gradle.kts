6728429201d47bbd75cb6ec3c0c4129876c88450
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-28 12:47:25 +0300

    feature : add a spring boot starter validation
    * Added a Spring Boot Validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 
 	compileOnly("org.mapstruct:mapstruct:1.6.3")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")

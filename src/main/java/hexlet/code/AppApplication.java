3b806b579288dfecbdad485a24ff83892299fe48
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-28 14:26:18 +0300

    feature : add the initializer database
    * Added the initializer database

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,13 +1,29 @@
 package hexlet.code;
 
+import hexlet.code.model.User;
+import hexlet.code.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    public AppApplication(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setEmail("hexlet@example.com");
+        user.setPassword("qwerty");
+        userRepository.save(user);
+    }
 }

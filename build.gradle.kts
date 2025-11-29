7a229c16434641b5c417fcde3c096afb069eed75
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-29 11:22:54 +0300

    feature : add an oauth2 resource server dependency
    * Added the OAUTH2 Resource Server

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
 	compileOnly("org.mapstruct:mapstruct:1.6.3")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")

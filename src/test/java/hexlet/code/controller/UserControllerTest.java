cb1df91958b54f7a8ff893823e9589fd811ef5c2
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-12-02 17:40:57 +0300

    feature : add an intergration tests
    * Added the integration tests

---
+++ src/test/java/hexlet/code/controller/UserControllerTest.java
@@ -0,0 +1,75 @@
+package hexlet.code.controller;
+
+import hexlet.code.model.User;
+import hexlet.code.repository.UserRepository;
+import hexlet.code.util.ModelGenerator;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest(
+        properties = {
+                "spring.jpa.defer-datasource-initialization=false",
+                "spring.sql.init.mode=never"
+        }
+)
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ModelGenerator modelGenerator;
+
+    private User testUser;
+
+    @BeforeEach
+    public void beforeEach() {
+        testUser = Instancio.of(modelGenerator.getUserModel())
+                .create();
+        userRepository.save(testUser);
+    }
+
+    @Test
+    @DisplayName("Test find all")
+    public void findAllTest() throws Exception {
+        mockMvc.perform(get("/api/users")
+                        .with(SecurityMockMvcRequestPostProcessors.user("admin")))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("Test delete by id")
+    public void deleteByIdTest() throws Exception {
+        mockMvc.perform(delete("/api/users/{id}", testUser.getId())
+                        .with(SecurityMockMvcRequestPostProcessors.user(testUser.getEmail())))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("Test find by id")
+    public void findByIdTest() throws Exception {
+        mockMvc.perform(get("/api/users/{id}", testUser.getId())
+                        .with(SecurityMockMvcRequestPostProcessors.user("user")))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}

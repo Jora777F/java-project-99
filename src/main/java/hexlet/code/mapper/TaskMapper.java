4eb3a356fec8b5e4727baf03d493b5ab6cc29e5a
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-12-02 16:34:54 +0300

    update : edit the task mapper
    * Edited the task mapper

--- src/main/java/hexlet/code/mapper/TaskMapper.java
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -2,7 +2,9 @@ package hexlet.code.mapper;
 
 import hexlet.code.dto.task.TaskRequestDto;
 import hexlet.code.dto.task.TaskResponseDto;
+import hexlet.code.model.Label;
 import hexlet.code.model.Task;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -11,17 +13,19 @@ import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface TaskMapper {
 
-    //TODO: Исправить замечания, связанные с labels
-
     @Mapping(source = "content", target = "description")
     @Mapping(source = "title", target = "name")
     @Mapping(target = "taskStatus", ignore = true)
     @Mapping(target = "assignee", ignore = true)
+    @Mapping(target = "labels", ignore = true)
     Task toTask(TaskRequestDto taskRequestDto);
 
     @InheritConfiguration(name = "toTask")
@@ -31,6 +35,17 @@ public interface TaskMapper {
     @Mapping(source = "description", target = "content")
     @Mapping(source = "name", target = "title")
     @Mapping(source = "assignee.id", target = "assigneeId")
+    @Mapping(target = "taskLabelIds", expression = "java(labelsToLabelIds(task.getLabels()))")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
     TaskResponseDto toTaskResponse(Task task);
 
+    default Set<Long> labelsToLabelIds(Set<Label> labels) {
+        if (labels == null) {
+            return Set.of();
+        }
+
+        return labels.stream()
+                .map(Label::getId)
+                .collect(Collectors.toSet());
+    }
 }

3ad901a8b73e4398a5820ffa6ed6a42adbad6f8c
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-12-03 16:42:27 +0300

    feature : add a sentry test controller
    * Added a sentry test controller

---
+++ src/main/java/hexlet/code/controller/sentry/SentryTestController.java
@@ -0,0 +1,21 @@
+package hexlet.code.controller.sentry;
+
+import io.sentry.Sentry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class SentryTestController {
+
+    @GetMapping("/sentry-test")
+    public String triggerException() {
+        try {
+            throw new RuntimeException("Test Sentry exception");
+        } catch (Exception e) {
+            Sentry.captureException(e);
+            return "Error captured and sent to Sentry!";
+        }
+    }
+}

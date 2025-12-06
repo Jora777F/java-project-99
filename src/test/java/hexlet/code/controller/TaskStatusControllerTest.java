7e3861f5b4b40e0b9fd56a08ee162fbfc3352b9f
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-12-06 16:17:29 +0300

    fix : fix the integration test

--- src/test/java/hexlet/code/controller/TaskStatusControllerTest.java
+++ src/test/java/hexlet/code/controller/TaskStatusControllerTest.java
@@ -1,6 +1,9 @@
 package hexlet.code.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.dto.task.status.TaskStatusResponse;
+import hexlet.code.mapper.TaskStatusMapper;
 import hexlet.code.model.TaskStatus;
 import hexlet.code.repository.TaskStatusRepository;
 import org.instancio.Instancio;
@@ -12,11 +15,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -38,6 +44,9 @@ class TaskStatusControllerTest extends BaseControllerTest {
 
     @Autowired
     private TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private TaskStatusMapper taskStatusMapper;
 
     private TaskStatus testTaskStatus;
 
@@ -62,10 +71,25 @@ class TaskStatusControllerTest extends BaseControllerTest {
     @Test
     @DisplayName("Should return status code 200, when find all task status.")
     public void findAllTaskStatus() throws Exception {
-        mockMvc.perform(get("/api/task_statuses")
+        MockHttpServletResponse response = mockMvc.perform(get("/api/task_statuses")
                         .with(SecurityMockMvcRequestPostProcessors.user("user")))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andReturn()
+                .getResponse();
+
+        String responseBody = response.getContentAsString();
+
+        List<TaskStatusResponse> actualDto = objectMapper.readValue(responseBody, new TypeReference<>() { });
+
+        List<TaskStatus> dbStatuses = taskStatusRepository.findAll();
+        List<TaskStatusResponse> expectedDto = dbStatuses
+                .stream()
+                .map(taskStatusMapper::toDto)
+                .toList();
+
+        assertThat(actualDto)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedDto);
     }
 
     @Test

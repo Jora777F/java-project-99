cf942b147913c8ac6c3da50420a434af03a31a92
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-12-06 19:06:56 +0300

    feature : add a join fetch to repository
    * Added the JOIN FETCH to the task repository

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -3,11 +3,16 @@ package hexlet.code.repository;
 import hexlet.code.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
     Optional<Task> findByName(String taskName);
+
+    @Query("SELECT DISTINCT t FROM Task t LEFT JOIN FETCH t.taskStatus LEFT JOIN FETCH t.assignee LEFT JOIN FETCH t.labels")
+    List<Task> findAllWithAssociations();
 }
